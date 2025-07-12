eaf6f3b153bca25fc4e033cbae6f4ad4762ffba0
opkhan27 <opkhan27@umd.edu>
2025-07-12 18:12:02 -0400

    Update vite config to redirect RTK query to appropriate service API

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
 
@@ -18,8 +17,13 @@ export default defineConfig(({ mode }) => {
     },
     server: {
       proxy: {
-        '/api': {
-          target: env.VITE_API_URL_ROOT,
+        '/api/parser': {
+          target: env.VITE_API_URL_IMAGE_PARSER,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, '/api/v1')
+        },
+        '/api/analyzer': {
+          target: env.VITE_API_URL_IMAGE_ANALYZER,
           changeOrigin: true,
           rewrite: (path) => path.replace(/^\/api/, '/api/v1')
         }
