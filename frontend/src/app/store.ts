5f06a1682581fdb1521a2a68774c8ac463730c2e
opkhan27 <opkhan27@umd.edu>
2025-07-13 17:30:38 -0400

    Fix analyzer proxy redirect

--- frontend/src/app/store.ts
+++ frontend/src/app/store.ts
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { parseImageApi } from "../services/parseImageApi";
+import { parseImageApi } from "@/services/parseImageApi";
+import { analyzeTextApi } from "@/services/analyzeTextApi";
 
 export const store = configureStore({
   reducer: {
     [parseImageApi.reducerPath]: parseImageApi.reducer,
+    [analyzeTextApi.reducerPath]: analyzeTextApi.reducer,
   },
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(parseImageApi.middleware),
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(parseImageApi.middleware, analyzeTextApi.middleware),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
