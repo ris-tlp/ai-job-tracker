b9b8116c343e5c16e4b0fcb9c20767508d5ca4c2
opkhan27 <opkhan27@umd.edu>
2025-07-14 22:17:03 -0400

    Configure toast

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Toaster } from "sonner";
 import HomePage from "./pages/Home";
 import UploadPage from "./pages/Upload";
 
 function App() {
   return (
     <Router>
+      <Toaster position="top-right" richColors />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/upload" element={<UploadPage />} />
